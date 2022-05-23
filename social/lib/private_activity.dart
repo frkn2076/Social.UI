f03cdfb05e59926c216ddfa53f20713715e1d2aa
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2022-05-23 20:46:04 +0300

    created private activity page

---
+++ social/lib/private_activity.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:social/activity.dart';
+import 'package:social/profile.dart';
+
+import 'register.dart';
+
+class PrivateActivity extends StatelessWidget {
+  const PrivateActivity({Key? key}) : super(key: key);
+
+  static const String _title = 'Sample App';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          child: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text(_title),
+        actions: [
+          IconButton(
+            icon: const CircleAvatar(
+              radius: 20,
+              backgroundImage: AssetImage('assets/images/foto1.jpeg'),
+            ),
+            tooltip: 'Go to profile',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Profile(id: 1)),
+              );
+            },
+          ),
+        ],
+      ),
+      body: const MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: ListView(
+        children: List<Widget>.from(
+          List<int>.generate(10, (i) => i + 1)
+              .map(
+                (i) => GestureDetector(
+                  onTap: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Register()),
+                    )
+                  },
+                  child: GestureDetector(
+                    child: Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blueAccent)),
+                      child: Text(
+                        'Etkinlik $i \nDetaylar... \n ...',
+                        style: const TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 30),
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Activity(id: i)),
+                      );
+                    },
+                  ),
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
