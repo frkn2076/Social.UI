7ddf24ce1c644a7493d4ff8b967d1a78b9763968
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2022-11-11 02:57:07 +0300

    alert dialogs added to auth pages

--- social/lib/login.dart
+++ social/lib/login.dart
@@ -37,99 +37,129 @@ class MyStatefulWidget extends StatefulWidget {
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  bool _isAlertDialogOn = false;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.fromLTRB(10, 100, 10, 0),
-              child: const Text(
-                'Login',
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-            Container(height: 20),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
+      padding: const EdgeInsets.all(10),
+      child: _isAlertDialogOn
+          ? AlertDialog(
+              title: const Text('Wrong credentials'),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: const <Widget>[
+                    Text('Your username or password is wrong')
+                  ],
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ForgotPassword(),
-                  ),
-                );
-              },
-              child: const Text(
-                'Forgot Password',
-              ),
-            ),
-            Container(
-              height: 50,
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: ElevatedButton(
-                child: const Text('Login'),
-                onPressed: () {
-                  var response = Api()
-                      .login(emailController.text, passwordController.text);
-                  response.then(
-                    (isSuccess) {
-                      setState(() {
-                        if (isSuccess) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const PublicActivity()),
-                          );
-                        }
-                      });
-                    },
-                  );
-                },
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text('Does not have account?'),
+              actions: <Widget>[
                 TextButton(
+                  child: const Text('Close'),
+                  onPressed: () {
+                    setState(() {
+                      _isAlertDialogOn = false;
+                    });
+                  },
+                ),
+              ],
+            )
+          : ListView(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.fromLTRB(10, 100, 10, 0),
                   child: const Text(
-                    'Sign in',
+                    'Login',
                     style: TextStyle(fontSize: 20),
                   ),
+                ),
+                Container(height: 20),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                TextButton(
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const Register()),
+                      MaterialPageRoute(
+                        builder: (context) => const ForgotPassword(),
+                      ),
                     );
                   },
-                )
+                  child: const Text(
+                    'Forgot Password',
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: ElevatedButton(
+                    child: const Text('Login'),
+                    onPressed: () {
+                      var response = Api()
+                          .login(emailController.text, passwordController.text);
+                      response.then(
+                        (isSuccess) {
+                          setState(() {
+                            if (isSuccess) {
+                              _isAlertDialogOn = false;
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const PublicActivity()),
+                              );
+                            } else {
+                              setState(() {
+                                _isAlertDialogOn = true;
+                              });
+                            }
+                          });
+                        },
+                      );
+                    },
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text('Does not have account?'),
+                    TextButton(
+                      child: const Text(
+                        'Sign in',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Register()),
+                        );
+                      },
+                    )
+                  ],
+                ),
               ],
             ),
-          ],
-        ));
+    );
   }
 }
--- social/lib/register.dart
+++ social/lib/register.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:social/http/api.dart';
-import 'login.dart';
+import 'package:social/public_activity.dart';
 
 class Register extends StatelessWidget {
   const Register({Key? key}) : super(key: key);
@@ -35,83 +35,129 @@ class MyStatefulWidget extends StatefulWidget {
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  var condition = _condition.none;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10),
-      child: ListView(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.fromLTRB(10, 100, 10, 0),
-            child: const Text(
-              'Sign In',
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(height: 20),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Email',
+      child: condition == _condition.success
+          ? AlertDialog(
+              title: const Text('Success'),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: const <Widget>[
+                    Text('You have registered succesfully')
+                  ],
+                ),
               ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-            child: TextField(
-              obscureText: true,
-              controller: passwordController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-              ),
-            ),
-          ),
-          Container(height: 40),
-          Container(
-            height: 50,
-            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-            child: ElevatedButton(
-              child: const Text('Sign in'),
-              onPressed: () {
-                var response = Api()
-                    .register(emailController.text, passwordController.text);
-                response.then(
-                  (isSuccess) {
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('Ok'),
+                  onPressed: () {
                     setState(() {
-                      if (isSuccess) {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const Login()),
-                        );
-                      }
+                      condition = _condition.none;
                     });
                   },
-                );
-              },
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('Have account?'),
-              TextButton(
-                child: const Text(
-                  'Login',
-                  style: TextStyle(fontSize: 20),
                 ),
-                onPressed: () {},
-              )
-            ],
-          ),
-        ],
-      ),
+              ],
+            )
+          : condition == _condition.fail
+              ? AlertDialog(
+                  title: const Text('Fail'),
+                  content: SingleChildScrollView(
+                    child: ListBody(
+                      children: const <Widget>[Text('Something went wrong')],
+                    ),
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      child: const Text('Close'),
+                      onPressed: () {
+                        setState(() {
+                          condition = _condition.none;
+                        });
+                      },
+                    ),
+                  ],
+                )
+              : ListView(
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.fromLTRB(10, 100, 10, 0),
+                      child: const Text(
+                        'Sign In',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Container(height: 20),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: TextField(
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Email',
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      child: TextField(
+                        obscureText: true,
+                        controller: passwordController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                        ),
+                      ),
+                    ),
+                    Container(height: 40),
+                    Container(
+                      height: 50,
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: ElevatedButton(
+                        child: const Text('Sign in'),
+                        onPressed: () {
+                          var response = Api().register(
+                              emailController.text, passwordController.text);
+                          response.then(
+                            (isSuccess) {
+                              setState(() {
+                                if (isSuccess) {
+                                  condition = _condition.success;
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => const PublicActivity()),
+                                  );
+                                } else {
+                                  condition = _condition.fail;
+                                }
+                              });
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const Text('Have account?'),
+                        TextButton(
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
+                  ],
+                ),
     );
   }
 }
+
+enum _condition { none, success, fail }
