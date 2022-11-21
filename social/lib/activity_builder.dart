585f4acd9d949c66e09d814f77a0db0943368fa0
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2022-11-21 14:18:35 +0300

    activity builder design changes

--- social/lib/activity_builder.dart
+++ social/lib/activity_builder.dart
@@ -84,207 +84,150 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               : ListView(
                   children: <Widget>[
                     Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.fromLTRB(50.0, 40.0, 50.0, 0),
-                      child: const Text(
-                        "Title:",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.fromLTRB(50.0, 0, 50.0, 0),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.blueAccent),
-                      ),
-                      child: TextField(controller: _titleController),
-                    ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.fromLTRB(50.0, 40.0, 50.0, 0),
-                      child: const Text(
-                        "Detail:",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.fromLTRB(50.0, 0, 50.0, 0),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.blueAccent),
-                      ),
-                      child: TextField(controller: _detailController),
-                    ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.fromLTRB(50.0, 40.0, 50.0, 0),
-                      child: const Text(
-                        "Location:",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.fromLTRB(50.0, 0, 50.0, 0),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.blueAccent),
-                      ),
-                      child: TextField(controller: _locationController),
-                    ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.fromLTRB(50.0, 40.0, 50.0, 0),
-                      child: const Text(
-                        "Date:",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.fromLTRB(50.0, 0, 50.0, 0),
-                      height:
-                          MediaQuery.of(context).copyWith().size.height * 0.25,
-                      // MediaQuery.of(context).copyWith().size.height * 0.25,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.blueAccent),
-                      ),
-                      child: CupertinoDatePicker(
-                        mode: CupertinoDatePickerMode.date,
-                        maximumYear: 2030,
-                        minimumYear: 2022,
-                        initialDateTime: _initDate,
-                        onDateTimeChanged: (DateTime value) {
-                          year = value.year;
-                          month = value.month;
-                          day = value.day;
-                        },
-                        use24hFormat: false,
-                        minuteInterval: 1,
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.fromLTRB(50.0, 40.0, 50.0, 0),
-                      child: const Text(
-                        "Time:",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.fromLTRB(50.0, 0, 50.0, 0),
-                      height:
-                          MediaQuery.of(context).copyWith().size.height * 0.25,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.blueAccent),
-                      ),
-                      child: CupertinoDatePicker(
-                        mode: CupertinoDatePickerMode.time,
-                        initialDateTime: _initDate,
-                        onDateTimeChanged: (DateTime value) {
-                          hour = value.hour;
-                          minute = value.minute;
-                        },
-                        use24hFormat: true,
-                        minuteInterval: 15,
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.fromLTRB(50.0, 40.0, 50.0, 0),
-                      child: const Text(
-                        "Phone Number:",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.fromLTRB(50.0, 0, 50.0, 0),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.blueAccent),
-                      ),
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
                       child: TextField(
-                          controller: _phoneNumberController,
-                          keyboardType: TextInputType.number),
-                    ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.fromLTRB(50.0, 40.0, 50.0, 0),
-                      child: const Text(
-                        "Capacity:",
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                        controller: _titleController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Title',
+                        ),
                       ),
                     ),
                     Container(
-                      alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.fromLTRB(50.0, 0, 50.0, 0),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.blueAccent),
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+                      child: TextField(
+                        controller: _detailController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Detail',
+                        ),
                       ),
-                      child: NumberPicker(
-                        value: _currentCapacity,
-                        minValue: 0,
-                        maxValue: 100,
-                        axis: Axis.horizontal,
-                        itemWidth: 50,
-                        onChanged: (value) =>
-                            setState(() => _currentCapacity = value),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+                      child: TextField(
+                        controller: _locationController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Location',
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+                      height:
+                          MediaQuery.of(context).copyWith().size.height * 0.20,
+                      child: InputDecorator(
+                        decoration: InputDecoration(
+                          labelText: 'Date',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        child: CupertinoDatePicker(
+                          mode: CupertinoDatePickerMode.date,
+                          maximumYear: 2030,
+                          minimumYear: 2022,
+                          initialDateTime: _initDate,
+                          onDateTimeChanged: (DateTime value) {
+                            year = value.year;
+                            month = value.month;
+                            day = value.day;
+                          },
+                          use24hFormat: false,
+                          minuteInterval: 1,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+                      height:
+                          MediaQuery.of(context).copyWith().size.height * 0.20,
+                      child: InputDecorator(
+                        decoration: InputDecoration(
+                          labelText: 'Time',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        child: CupertinoDatePicker(
+                          mode: CupertinoDatePickerMode.time,
+                          initialDateTime: _initDate,
+                          onDateTimeChanged: (DateTime value) {
+                            hour = value.hour;
+                            minute = value.minute;
+                          },
+                          use24hFormat: true,
+                          minuteInterval: 15,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+                      child: TextField(
+                        controller: _phoneNumberController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'PhoneNumber',
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+                      height:
+                          MediaQuery.of(context).copyWith().size.height * 0.20,
+                      child: InputDecorator(
+                        decoration: InputDecoration(
+                          labelText: 'Capacity',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        child: NumberPicker(
+                          value: _currentCapacity,
+                          minValue: 0,
+                          maxValue: 100,
+                          axis: Axis.horizontal,
+                          itemWidth: 50,
+                          onChanged: (value) =>
+                              setState(() => _currentCapacity = value),
+                        ),
                       ),
                     ),
                     Container(
                       alignment: Alignment.topCenter,
-                      padding: const EdgeInsets.all(5),
-                      margin: const EdgeInsets.fromLTRB(50.0, 20.0, 50.0, 0),
+                      padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.blueAccent),
-                            ),
-                            child: ElevatedButton(
-                              child: const Text("Save"),
-                              onPressed: () {
-                                var dateInput = DateFormat('yyyy-MM-ddTHH:mm')
-                                    .format(DateTime(
-                                        year, month, day, hour, minute));
-                                Api()
-                                    .createActivity(
-                                        _titleController.text,
-                                        _detailController.text,
-                                        _locationController.text,
-                                        dateInput,
-                                        _phoneNumberController.text,
-                                        _currentCapacity)
-                                    .then((isSuccess) {
-                                  setState(() =>
-                                      _condition = isSuccess.conditionParser());
-                                  if (isSuccess) {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            const PublicActivity(),
-                                      ),
-                                    );
-                                  }
-                                });
-                              },
-                            ),
+                          ElevatedButton(
+                            child: const Text("Save"),
+                            onPressed: () {
+                              var dateInput = DateFormat('yyyy-MM-ddTHH:mm')
+                                  .format(DateTime(
+                                      year, month, day, hour, minute));
+                              Api()
+                                  .createActivity(
+                                      _titleController.text,
+                                      _detailController.text,
+                                      _locationController.text,
+                                      dateInput,
+                                      _phoneNumberController.text,
+                                      _currentCapacity)
+                                  .then((isSuccess) {
+                                setState(() =>
+                                    _condition = isSuccess.conditionParser());
+                                if (isSuccess) {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const PublicActivity(),
+                                    ),
+                                  );
+                                }
+                              });
+                            },
                           ),
                         ],
                       ),
